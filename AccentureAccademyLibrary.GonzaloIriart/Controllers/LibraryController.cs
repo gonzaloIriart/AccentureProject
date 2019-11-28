59cd1519739e17080b5df2ec1e3fec1d9c60a29e
T11641.Training10 <T11641.Training10@CPX-T1VCT9YEC7D>
2019-11-28 17:44:24 -0300

    forms

--- AccentureAccademyLibrary.GonzaloIriart/Controllers/LibraryController.cs
+++ AccentureAccademyLibrary.GonzaloIriart/Controllers/LibraryController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,16 +15,34 @@ namespace AccentureAccademyLibrary.GonzaloIriart.Controllers
         {
             this.db = new AccentureAccademyLibraryEntities();
         }
-        // GET: Library
-        public ActionResult Index()
-        {
 
-            return View();
+
+        // GET: Library
+        public ActionResult JsonListBooks()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Book> books = this.db.Book.ToList();
+
+            return Json(books, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult AddBook()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Agregar(Book book, IEnumerable<int> autores)
+        {
+            foreach (int autorActual in autores)
+            {
+                Author autor = this.db.Author.Find(autorActual);
+                book.Author.Add(autor);
+            }
+            db.Book.Add(book);
+            db.SaveChanges();
+            return Content("Libro añadido satisfactoriamente");
+        }
+
     }
 }
