0e38fce738a34a5d7a3c92aaa83f602b9c4a9dc3
T11641.Training10 <T11641.Training10@CPX-T1VCT9YEC7D>
2019-11-29 12:52:12 -0300

    added views for new data

--- AccentureAccademyLibrary.GonzaloIriart/Controllers/LibraryController.cs
+++ AccentureAccademyLibrary.GonzaloIriart/Controllers/LibraryController.cs
@@ -74,5 +74,47 @@ namespace AccentureAccademyLibrary.GonzaloIriart.Controllers
             return Content("Libro añadido satisfactoriamente");
         }
 
+        public ActionResult Publisher()
+        {
+            ViewBag.Name = "Publisher";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Publisher(Publisher p)
+        {
+            db.Publisher.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("AddBook");
+        }
+
+        public ActionResult Author()
+        {
+            ViewBag.Name="Author";
+            return View("Publisher");
+        }
+
+        [HttpPost]
+        public ActionResult Author(Author Author)
+        {
+            this.db.Author.Add(Author);
+            this.db.SaveChanges();
+            return RedirectToAction("AddBook");
+        }
+
+        public ActionResult Genre()
+        {
+            ViewBag.Name = "Genre";
+            return View("Publisher");
+        }
+
+        [HttpPost]
+        public ActionResult Genre(Genre Genre)
+        {
+            this.db.Genre.Add(Genre);
+            this.db.SaveChanges();
+            return RedirectToAction("AddBook");
+        }
+
     }
 }
